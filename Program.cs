0bcc9e4d7077049633889c6dcd0d4fe7843e89e6
MixterVee <mixtervandal@gmail.com>
2026-09-07 10:52:10 -0400

    Attach secure remote control hub

--- Program.cs
+++ Program.cs
@@ -11,6 +11,7 @@ internal static class Program
         StartButtonAccent.Apply(form);
         DestinationQueueSync.Attach(form);
         QueueEditingIntegration.Attach(form);
+        RemoteControlIntegration.Attach(form);
         RemoteMonitorIntegration.Attach(form);
         RemoteRelayIntegration.Attach(form);
         Application.Run(form);
