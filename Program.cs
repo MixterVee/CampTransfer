7c88e0414d82ebdc38c1ac654acfe84da1b5bbca
MixterVee <mixtervandal@gmail.com>
2026-09-07 10:49:30 -0400

    Attach queue editing controls

--- Program.cs
+++ Program.cs
@@ -10,6 +10,7 @@ internal static class Program
         var form = new MainForm();
         StartButtonAccent.Apply(form);
         DestinationQueueSync.Attach(form);
+        QueueEditingIntegration.Attach(form);
         RemoteMonitorIntegration.Attach(form);
         RemoteRelayIntegration.Attach(form);
         Application.Run(form);
