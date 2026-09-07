bd985e81f03e42b4b513624aff0cbc8533bfb7a3
MixterVee <mixtervandal@gmail.com>
2026-09-07 00:25:37 -0400

    Bump CampTransfer Remote to v0.2.0

--- android/CampTransferRemote/app/build.gradle.kts
+++ android/CampTransferRemote/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.mixtervee.camptransferremote"
         minSdk = 26
         targetSdk = 32
-        versionCode = 3
-        versionName = "0.1.2"
+        versionCode = 4
+        versionName = "0.2.0"
     }
 
     buildTypes {
