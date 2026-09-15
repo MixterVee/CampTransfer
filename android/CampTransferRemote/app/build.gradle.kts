6696676b6f96fdae2b87134a659d78cd476f1e15
MixterVee <mixtervandal@gmail.com>
2026-09-15 16:58:28 -0400

    Bump Android recovery build to 0.4.3

--- android/CampTransferRemote/app/build.gradle.kts
+++ android/CampTransferRemote/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.mixtervee.camptransferremote"
         minSdk = 26
         targetSdk = 32
-        versionCode = 11
-        versionName = "0.4.2"
+        versionCode = 12
+        versionName = "0.4.3"
     }
 
     buildTypes {
